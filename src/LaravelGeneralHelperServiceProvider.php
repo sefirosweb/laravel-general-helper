ca7ae8822bffdc5e4b4c34f09f8530b2388d7ae1
Javier García <javier.garcia@decowood.es>
2022-02-19 17:03:48 +0100

    Adding composer and service provider

---
+++ src/LaravelGeneralHelperServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sefirosweb\LaravelGeneralHelper;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGeneralHelperServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+
+    public function register()
+    {
+    }
+}
